a9ed1a7de4447e8e2c5141d37948838032193f44
milena-git <milena.suarez-castillo@insee.fr>
2021-06-07 18:13:51 +0200

    Read population data in the repo

--- R/maps_utils.R
+++ R/maps_utils.R
@@ -17,8 +17,8 @@ departement_centroids <- function() {
 
 get_residents <- function(){
   # --- Official number of residents. 
-  httr::GET("https://www.insee.fr/fr/statistiques/fichier/2012692/TCRD_021.xls", httr::write_disk(tf <- tempfile(fileext = ".xls")))
-  res <- readxl::read_excel(tf, skip = 4, n_max = 96, col_names = c('code','name','pop','part_fem','part_hom','part_age1','part_age2','part_age3','part_age4'))
+  res <- read.csv("data/pop2020.csv")
+  colnames(res) <- c('code',"name","pop")
   res$code[res$code %in% c('2A','2B')] <- '2AB'
   res <- res %>% group_by(code) %>% summarise(popRes = round(sum(pop),-3))
   return(res)
